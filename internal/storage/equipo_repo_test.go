e603780024d98eade40493f718b307c9667a8437
NathalyLucas11 <natylucas@gmail.com>
2026-07-09 21:48:25 -0500

    test(storage/equipo): add base equipo_repo_test.go with DB helper

---
+++ internal/storage/equipo_repo_test.go
@@ -0,0 +1,19 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/glebarez/sqlite"
+	"github.com/stretchr/testify/require"
+	"gorm.io/gorm"
+
+	"pool-api/internal/models"
+)
+
+func abrirDBEquipo(t *testing.T) *gorm.DB {
+	t.Helper()
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	require.NoError(t, err)
+	require.NoError(t, db.AutoMigrate(&models.Equipo{}))
+	return db
+}
